7edca410fc56be285e61b5264cd3413100962f53
PopescuIoanaLigia <151943591+PopescuIoanaLigia@users.noreply.github.com>
2024-10-09 16:27:27 +0000

    Lab 1 PCLP4 soft

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,20 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
-    // TODO
+    // let mut max = 0;
+    // let mut min = 99;
+    // for i in input {
+    //     if i> max{
+    //         max = i;
+    //     }
+    //     if i< min{
+    //         min = i;
+    //     }
+    // }
 
-    println!("{} is largest and {} is smallest");
+
+    let min = input.iter().min().unwrap_or(&0);
+    let max = input.iter().max().unwrap_or(&0);
+
+    println!("{:?} is largest and {:?} is smallest", max, min);
 }
